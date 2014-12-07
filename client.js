8e481d75ecc335dba21b39abd6ec37d4363814f8
Kiko Herrschaft <kiko.herrschaft@gmail.com>
2014-12-07 02:06:57 -0200

    kill sync, sync check on refresh and bidirectional sync

--- client.js
+++ client.js
@@ -3,16 +3,22 @@
 // Since I cannot find a way to emit events from server to server, 
 // I'm using this client script to mirror events and send them back to server.
 // model -> socket.emit('sync:toServer:event');
-var socket = io.connect();
-socket.on('message', function(message, data){
-	data = !data ? "" : data;
-	try {
-		var path = message.split(':');
-		if(path[0] == 'sync'){
-			// Sending back to server
-			if(path[1] == 'toServer'){
-				socket.emit(path[2], data);
+
+// Append here all namespaces used, or left only '/' for the default.
+var namespaces = ['/', '/customRoom'];
+
+namespaces.forEach(function(namespace){
+	var socket = io.connect(namespace);
+	socket.on('message', function(message, data){
+		data = !data ? "" : data;
+		try {
+			var path = message.split(':');
+			if(path[0] == 'sync'){
+				// Sending back to server
+				if(path[1] == 'toServer'){
+					socket.emit(path[2], data);
+				}
 			}
-		}
-	} catch(err){}
+		} catch(err){}
+	});
 });
